6250b9b93741d4a6fef851988547c2f4060b54f4
Kaouther Wafa Mouici <kaouther-wafa.mouici@etu.u-paris.fr>
2025-01-05 22:43:39 +0100

    changement fonction principale

--- boite_noire_final.py
+++ boite_noire_final.py
@@ -11,23 +11,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-## Allumage et acquisition d'images
-slm = slmdisplaysdk.SLMInstance()
-
-## Check if the library implements the required version
-if not slm.requiresVersion(5):
-    exit(1)
-
-#ouvre le SLM et renvoie une erreur si la connexion a échouée
-error = slm.open()
-assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
-
-#connexion à la camera (si besoin)
-testbed = TestbedProxy('127.0.0.1', 8768)               # Establish connection to the testbed server.
-
-cam = testbed.corono_camera                           # Start up / connect to the camera.
-cam.exposure_time = 109974.681  #pour 633nm
-
 ''' Before Starting'''
 ''' On ERIS CONFIGURATION MANAGER you need to load the configuration file : ERISCalibration_NIR-153_800.00nm_2.00pi_sgl=1540.hecalib.txt'''
 
@@ -42,36 +25,45 @@ def voltage_deg1(phase, alpha, beta):
     '''voltage à l'ordre 1'''
     return (phase - alpha) / beta
 
-
 #on crée un exemple de carte de phase : tableau numpy flottant
 tableau_phase =np.zeros((1200,1920)).astype('float64')
 tableau_phase[300:600,:]=1.3*np.pi
 tableau_phase[900:1200,:]=np.pi
 
-#Fonction qui prendra un fichier contenant la carte de phase, longueur d'onde du laser et renvoie un tableau du taux de voltage à appliquer
-def carte_de_voltage(carte_phase, longueur_d_onde):    
+def applique_une_phase_au_slm(carte_phase, longueur_d_onde):
+    """
+    Cette fonction prend une carte de phase et la longueur d'onde du laser,
+    génère une carte de voltage appropriée, et envoie cette carte au SLM.
 
-    dimension_carte = carte_phase.shape
-    
-    #dimension SLM [1920 x 1200]
-    ligne_SLM = 1200
-    colonne_SLM = 1920
+    Parameters
+    ----------
+    carte_phase : numpy.ndarray
+        Carte de phase qui doit être appliquée au SLM.
+    longueur_d_onde : int
+        Longueur d'onde du laser en nanomètres.
 
-    if dimension_carte[0] != ligne_SLM or dimension_carte[1] != colonne_SLM :
+    Returns
+    -------
+    None
+    """
+    # Allumage du SLM
+    slm = slmdisplaysdk.SLMInstance()
+    # Vérifier si la librairie implémente la bonne version
+    if not slm.requiresVersion(5):
+        exit(1)
+    # Ouverture du SLM et renvoie une erreur si la connexion a échoué
+    error = slm.open()
+    assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
+
+    # Vérifier la dimension de la carte de phase
+    if carte_phase.shape != (1200, 1920):
         raise ValueError("La carte de phase doit être de même dimension que le SLM (1920 x 1200)")
 
-    #moduler toutes les valeurs de phase à 2pi si les valeurs sont supérieures à 2pi
-    #carte_phase = np.mod(carte_phase, 2*np.pi)
-
-    #on ne choisit qu'un seul étalonnage pour toutes les longueurs d'onde : 800nm 2pi
-    #ouverture et lecture du fichier d'étalonnage correspondant au longueur_d_onde
-    #changer l'adresse si les fichiers on changé de dossier
+    # Ouverture des fichiers d'étalonnage et extraction des coefficients
     fichier_633 = "./coeff_ordre2/633_800_2pi_ordre2.fits"
     fichier_705 = "./coeff_ordre2/705_800_2pi_ordre2.fits"
     fichier_785 = "./coeff_ordre2/785_800_2pi_ordre2.fits"
     
-
-    #ouverture de chaque fichier d'étalonnage 
     with fits.open(fichier_633) as hdul:
         data_633 = hdul[1].data
     with fits.open(fichier_705) as hdul:
@@ -79,56 +71,35 @@ def carte_de_voltage(carte_phase, longueur_d_onde):
     with fits.open(fichier_785) as hdul:
         data_785 = hdul[1].data
 
-    # extraction des coefficients alpha, beta et gamma
     alpha_633, beta_633, gamma_633 = data_633[2][1], data_633[3][1], data_633[4][1]
     alpha_705, beta_705, gamma_705 = data_705[2][1], data_705[3][1], data_705[4][1]
     alpha_785, beta_785, gamma_785 = data_785[2][1], data_785[3][1], data_785[4][1]
-    
-    #conversion phase --> voltage 
-    #ici à l'ordre 1
+
+    # Calcul du voltage basé sur la longueur d'onde
     if 633 <= longueur_d_onde < 705:
-        V = (705 - longueur_d_onde) / (705 - 633) * voltage(tableau_phase + alpha_633, alpha_633, beta_633, gamma_633) + (
-                    longueur_d_onde - 633) / (705 - 633) * voltage_deg1(tableau_phase + alpha_705, alpha_705, beta_705)
+        V = (705 - longueur_d_onde) / (705 - 633) * voltage(carte_phase + alpha_633, alpha_633, beta_633, gamma_633) + \
+            (longueur_d_onde - 633) / (705 - 633) * voltage_deg1(carte_phase + alpha_705, alpha_705, beta_705)
+    elif 705 <= longueur_d_onde < 785:
+        V = (785 - longueur_d_onde) / (785 - 705) * voltage(carte_phase + alpha_705, alpha_705, beta_705, gamma_705) + \
+            (longueur_d_onde - 705) / (785 - 705) * voltage(carte_phase + alpha_785, alpha_785, beta_785, gamma_785)
+    else:
+        V = voltage(carte_phase + alpha_785, alpha_785, beta_785, gamma_785)
 
-    if 705 <= longueur_d_onde < 785:
-        V = (785 - longueur_d_onde) / (785 - 705) * voltage(tableau_phase + alpha_705, alpha_705, beta_705, gamma_705) + (
-                    longueur_d_onde - 705) / (785 - 705) * voltage(tableau_phase + alpha_785, alpha_785, beta_785, gamma_785)
-
-    if longueur_d_onde >= 785:
-        V = voltage(tableau_phase + alpha_785, alpha_785, beta_785, gamma_785)
-
-    
-    # Création de deux plots côte à côte : carte de phase et carte de voltage
+    # Affichage des cartes
     fig, axes = plt.subplots(1, 2, figsize=(16, 6))
-
-    # Plot de la carte de phase
     im1 = axes[0].imshow(carte_phase, cmap='gray_r', origin='lower', aspect='auto', vmin=0, vmax=2*np.pi)
     axes[0].set_title("Carte de Phase pour SLM")
-    axes[0].set_xlabel("Pixels (X)")
-    axes[0].set_ylabel("Pixels (Y)")
-    plt.colorbar(im1, ax=axes[0], fraction=0.046, pad=0.04, label='Phase (radians)')
-
-    # Plot de la carte de voltage
     im2 = axes[1].imshow(V, cmap='viridis', origin='lower', aspect='auto', vmin=0, vmax=1)
     axes[1].set_title("Carte de Voltage pour SLM")
-    axes[1].set_xlabel("Pixels (X)")
-    axes[1].set_ylabel("Pixels (Y)")
-    plt.colorbar(im2, ax=axes[1], fraction=0.046, pad=0.04, label='Voltage (%)')
-
-    # Ajustement de l'espacement
-    plt.tight_layout(pad=2.0, w_pad=3.0)
-    # Ou utilisez plt.subplots_adjust(wspace=0.3)
-
-    # Affichage des plots
+    plt.colorbar(im1, ax=axes[0], label='Phase (radians)')
+    plt.colorbar(im2, ax=axes[1], label='Voltage (%)')
+    plt.tight_layout()
     plt.show()
-    
-    return V.astype('float64')
+
+    # Envoyer la carte de voltage au SLM
+    error = slm.showData(V.astype('float64'))  # Convertit V en float64 si ce n'est pas déjà fait
+    # Envoie un message d'erreur si un problème avec le SLM est rencontré
+    assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
+    print("La carte de phase a bien été envoyée au SLM")
 
 
-#test boite noire sans appliquer au SLM
-carte_de_voltage(tableau_phase, 705)
-
-
-#appliquer la carte de voltage au SLM
-error = slm.showData(carte_de_voltage((tableau_phase, 633)))
-assert error == slmdisplaysdk.ErrorCode.NoError, slm.errorString(error)
